451f087b62d636edd0d9c39c17fda196c446992b
Dany Ouaras <dany.ouaras.bbo@banctec.fr>
2012-06-15 16:30:01 +0200

    First Commit, Keylogger

---
+++ RecupKeYFile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	system("XCOPY \\\\439BTECFR\\Downloads\\Fichier.txt C:\\Documents");
+
+	system("pause");
+	return 0;
+}
